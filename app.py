c6be38b0f75e1e8bd567c7a624b6d3371ac36703
atulpatel8251 <100270477+atulpatel8251@users.noreply.github.com>
2024-06-24 15:45:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,8 +58,8 @@ images = ['6MarkQ']
 #os.environ["OPENAI_API_TYPE"] = ""
 #os.environ["OPENAI_API_VERSION"] = ""
 #os.environ["OPENAI_API_BASE"] = ""
-os.environ["OPENAI_API_KEY"] = "API_KEY"
-#openai_api_key = os.getenv('OPENAI_API_KEY')
+#os.environ["OPENAI_API_KEY"] = "API_KEY"
+openai_api_key = os.getenv('OPENAI_API_KEY')
 
 
 
