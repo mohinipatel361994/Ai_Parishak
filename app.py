5a59a3a412ee669fced5559c7077412480afbf75
atulpatel8251 <100270477+atulpatel8251@users.noreply.github.com>
2024-06-25 10:27:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -63,11 +63,10 @@ images = ['6MarkQ']
 #secret_value = environ.get('OPENAI_API_KEY')
 
 secret_key_environment_variable_name = 'API_ID'
-        secret_key_input = str(environ.get(secret_key_environment_variable_name)).upper()
-        if secret_key_input is None or len(secret_key_input) == 0:
-            raise ArgumentError(argument=None, message=f"Missing environment variable with key {secret_key_environment_variable_name}. It will be the Key to create the secret.")
-
-        secret_value = environ.get('OPENAI_API_KEY')
+secret_key_input = str(environ.get(secret_key_environment_variable_name)).upper()
+#if secret_key_input is None or len(secret_key_input) == 0:
+#raise ArgumentError(argument=None, message=f"Missing environment variable with key {secret_key_environment_variable_name}. It will be the Key to create the secret.")
+secret_value = environ.get('OPENAI_API_KEY')
 
 def get_base64(bin_file):
     with open(bin_file, 'rb') as f:
