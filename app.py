be746c588969bc004eeabb7c7a00c16f52e79631
atulpatel8251 <100270477+atulpatel8251@users.noreply.github.com>
2024-06-26 12:40:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,8 +58,6 @@ images = ['6MarkQ']
 
 openai_api_key = os.getenv("OPENAI_API_KEY")
 openai_api_key2 = st.secrets["secret_section"]["OPENAI_API_KEY"]
-print("==========================================")
-print(len(openai_api_key2))
 # Use the API key
 TEMP_MD_FILE = r"question.pdf"
 TEMP_PDF_FILE = "__temp.pdf"
