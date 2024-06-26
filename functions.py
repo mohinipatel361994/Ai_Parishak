5777a3d242ac9991e725c3be4e188686dde0ab6e
atulpatel8251 <100270477+atulpatel8251@users.noreply.github.com>
2024-06-26 12:14:12 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -41,8 +41,8 @@ def images_to_txt(path, language):
     print(os.getenv('poppler_path'))
     print("==================================")
     
-    #images = pdf2image.convert_from_bytes(path,poppler_path = r'poppler-24.02.0/Library/bin/')
-    images = pdf2image.convert_from_bytes(path)
+    images = pdf2image.convert_from_bytes(path,poppler_path = r'poppler-24.02.0/Library/bin/')
+    #images = pdf2image.convert_from_bytes(path)
     all_text = []
     for v,i in enumerate(images):
         pil_im = i
